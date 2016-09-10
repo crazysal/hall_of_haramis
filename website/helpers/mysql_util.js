906ed2613ccd489d3279a4e1ec6c36e89f08e551
nikas <nikas@faasos.com>
2016-09-10 23:59:19 +0530

    data

--- website/helpers/mysql_util.js
+++ website/helpers/mysql_util.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection(
 {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'jacksnip',
+  password: process.env.DB_PASS || 'root',
   database: "competetions",
   port: 3306
 });
