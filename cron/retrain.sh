7a5831d17948d99b44e03363260731baf30009bf
nikas <nikas@faasos.com>
2016-09-11 13:57:20 +0530

    final model comiited

--- cron/retrain.sh
+++ cron/retrain.sh
@@ -1,5 +1,5 @@
 rm /tmp/neg.txt
-mysql -uroot -proot comptetions -e ' select tweet_texts into outfile "/tmp/neg.txt" from tweets where human_label="neg" and is_trainable=1 and DATE(created_at)=DATE(NOW());'
+mysql -uroot -proot competetions -e ' select tweet_text into outfile "/tmp/neg.txt" from tweets where human_label="neg" and is_trainable=1 and DATE(created_at)=DATE(NOW());'
 
 rm /home/ubuntu/code/sequoia-hack/current/neg_new.txt
 cp /tmp/neg.txt /home/ubuntu/code/sequoia-hack/current/neg_new.txt
@@ -7,7 +7,7 @@ cp /tmp/neg.txt /home/ubuntu/code/sequoia-hack/current/neg_new.txt
 
 
 rm /tmp/pos.txt
-mysql -uroot -proot comptetions -e ' select tweet_texts into outfile "/tmp/pos.txt" from tweets where human_label="pos" and is_trainable=1 and DAT    E(created_at)=DATE(NOW());'
+mysql -uroot -proot competetions -e ' select tweet_texts into outfile "/tmp/pos.txt" from tweets where human_label="pos" and is_trainable=1 and DAT    E(created_at)=DATE(NOW());'
 rm /home/ubuntu/code/sequoia-hack/current/pos_new.txt
 cp /tmp/neg.txt /home/ubuntu/code/sequoia-hack/current/pos_new.txt
 
